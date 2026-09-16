6347901c3a74a634ee92e6db225b89fd6db22e29
sinchubhat <sinchana.s.r@intel.com>
2026-09-16 15:41:28 +0530

    test: fix deactivation unit test after merge

--- internal/commands/deactivate_test.go
+++ internal/commands/deactivate_test.go
@@ -825,6 +825,7 @@ func TestExecuteHttpConsoleDeactivate_MissingDeviceIsNotRecreated(t *testing.T) 
 
 	mockAMT := mock.NewMockInterface(ctrl)
 	mockAMT.EXPECT().GetUUID().Return("test-guid", nil)
+	mockAMT.EXPECT().GetUPID().Return(nil, nil)
 	mockAMT.EXPECT().GetOSDNSSuffix().Return("example.com", nil)
 
 	mockWSMAN := mock.NewMockWSMANer(ctrl)
